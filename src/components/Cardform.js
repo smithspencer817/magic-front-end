62195a3fc1125c7edfdae04198deec4300b309a1
MarkDLR <mark.p.dlr@gmail.com>
2020-08-20 15:45:55 -0500

    added patch to the submit

--- src/components/Cardform.js
+++ src/components/Cardform.js
@@ -5,25 +5,26 @@ export default class Cardform extends Component {
 
 
     render() {
+      const cardInfo = this.props.cardInfo
         
         return(
           <div className="cardform">
               Card Form
               <div id="preview">
-                Preview: <PreviewCard cardInfo={this.props.cardInfo} />
+                Preview: <PreviewCard cardInfo={cardInfo} />
                 </div>
-                <form onSubmit={this.props.handleSubmit}>
+                <form id="card-form" onSubmit={this.props.handleSubmit}>
                     <label htmlFor="name-input">Enter Name: </label>
-                    <input onChange={this.props.previewCard} id="name-input" type="text" name="name" placeholder="Enter Card Name" /><br></br>
+                    <input onChange={this.props.previewCard} id="name-input" type="text" name="name" placeholder="Enter Card Name" value={cardInfo.pName} /><br></br>
                     <label htmlFor="mana-input">Enter Mana: </label>
-                    <input onChange={this.props.previewCard} id="mana-input" type="text" name="mana_cost" placeholder="Mana Cost" /><br></br>
+                    <input onChange={this.props.previewCard} id="mana-input" type="text" name="mana_cost" placeholder="Mana Cost" value={cardInfo.pMana}/><br></br>
                     <label htmlFor="image-input">Enter Image Url: </label>
-                    <input onChange={this.props.previewCard} id="image-input" type="text" name="img_url" placeholder="Enter Card Image Url" /><br></br>
+                    <input onChange={this.props.previewCard} id="image-input" type="text" name="img_url" placeholder="Enter Card Image Url" value={cardInfo.pImg} /><br></br>
                     <label htmlFor="type-input">Enter Spell Type: </label>
-                    <input onChange={this.props.previewCard} id="type-input" type="text" name="spell_type" placeholder="Enter Spell Type" /><br></br>
+                    <input onChange={this.props.previewCard} id="type-input" type="text" name="spell_type" placeholder="Enter Spell Type" value={cardInfo.pType} /><br></br>
                     <label htmlFor="description-input">Enter Description: </label>
                     {/* <input id="description-input" type="text" name="description" placeholder="Enter Card Description" /><br></br> */}
-                    <textarea onChange={this.props.previewCard} id="description-input" name="description" placeholder="Enter Text Here"></textarea>
+                    <textarea onChange={this.props.previewCard} id="description-input" name="description" placeholder="Enter Text Here" value={cardInfo.pDesc} ></textarea>
                     <button type="submit">Submit New Card</button>
                 </form>
                 
